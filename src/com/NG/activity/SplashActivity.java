95728e556880a6aa65b8731393028a3fb8c5d681
Lihang Li <licalmer@gmail.com>
2013-12-27 20:24:19 +0800

    implementation of splash auto jump to main activity, default to 1s for now

--- src/com/NG/activity/SplashActivity.java
+++ src/com/NG/activity/SplashActivity.java
@@ -2,22 +2,17 @@ package com.NG.activity;
 
 
 import com.NG.moviesearchbeta.R;
-import com.NG.viewpager.ViewPagerActivity;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.DisplayMetrics;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
-
-
 
 public class SplashActivity extends Activity{
-	private ImageView imageview;
-	private TextView tv;
+	private final int SPLASH_DISPLAY_LENGHT = 1000; // Splash display time in ms
+	
+	//private ImageView imageview;
+	//private TextView tv;
 	private int displayWidth;
 	private int displayHeight;
 	@Override
@@ -36,12 +31,27 @@ public class SplashActivity extends Activity{
         System.out.println(density);
         
         setContentView(R.layout.splash);
+        
+        new Handler().postDelayed(new Runnable() {
+			public void run() {
+				Intent nextIntent = new Intent();
+				
+				nextIntent.setClass(SplashActivity.this, MainActivity.class);
+					
+				SplashActivity.this.startActivity(nextIntent);
+				SplashActivity.this.finish();
+			}
+
+		}, SPLASH_DISPLAY_LENGHT);
+        
         /*
 		tv = (TextView)findViewById(R.id.splash_text);
         tv.setText("your screen height = "+displayHeight +"\n"
         			+"width = "+displayWidth+"\n"
         			+"density = "+density);
         */
+        
+        /*
         imageview = (ImageView)findViewById(R.id.splash_img);
 		
 		imageview.setOnClickListener(new Button.OnClickListener(){
@@ -57,6 +67,7 @@ public class SplashActivity extends Activity{
 			}
 			
 		});
+		*/
 				
 	}
 	
